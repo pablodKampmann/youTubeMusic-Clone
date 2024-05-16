f2ff79c735ec0f746d466fe5febedf957f07c75c
pablodKampmann <pablo7kamp@gmail.com>
2024-05-16 14:15:24 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,53 @@
 import Image from "next/image";
-import ListenAgain from "./components/homeComponents/listenAgain";
-import QuickPicks from "./components/homeComponents/quickPicks";
+import ListenAgainDesktop from "./components/homeComponents/desktop/listenAgain";
+import ListenAgainMobile from "./components/homeComponents/mobile/listenAgain";
+import QuickPicks from "./components/homeComponents/desktop/quickPicks";
 
 export default function Home() {
   return (
-    <div className="h-screen  w-full pl-32 pr-28 overflow-y-auto flex-col bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#10100C] from-25% via-[#06080A] via-60% to-[#03050A] to-100%  ">
-      <div className=" pb-72">
-        <div className="w-full h-fit whitespace-nowrap space-x-3 flex text-sm overflow-y-hidden mt-12 container">
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Relax</button>
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Fiesta</button>
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Energía</button>
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Entretenimiento</button>
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Concentración</button>
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Triste</button>
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Amor</button>
-          <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Dormir</button>
+    <div>
+
+      {/* DESKTOP */}
+      <div className="h-screen desktop-version w-full pl-32 pr-28 overflow-y-auto flex-col bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#10100C] from-25% via-[#06080A] via-60% to-[#03050A] to-100%  ">
+
+        <div className=" pb-72 ">
+          <div className="w-full h-fit whitespace-nowrap space-x-3 focus:outline-none flex text-sm overflow-y-hidden mt-12 container">
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Relax</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Fiesta</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Energía</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Entretenimiento</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Concentración</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Triste</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Amor</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Dormir</button>
+          </div>
+
+          <ListenAgainDesktop />
+
+          <QuickPicks />
+        </div>
+      </div>
+
+
+      {/* MOBILE */}
+      <div className="h-screen mobile-version w-full px-4 overflow-y-auto flex-col bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#10100C] from-25% via-[#06080A] via-60% to-[#03050A] to-100%  ">
+
+        <div className="pb-72 ">
+          <div className="w-full  h-fit whitespace-nowrap  space-x-3 focus:outline-none flex text-sm overflow-y-hidden mt-10 container">
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Relax</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Fiesta</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Energía</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Entretenimiento</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Concentración</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Triste</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Amor</button>
+            <button className="bg-white bg-opacity-10 py-2 px-2.5 rounded-lg hover:bg-opacity-20 transition duration-150">Dormir</button>
+          </div>
+
+          <ListenAgainMobile />
+
         </div>
 
-        <ListenAgain />
-
-        <QuickPicks />
       </div>
 
     </div>
